b8a32d6f48990007d7ead23d81c2615883a4e4f6
nimkibet <nimrodkibet376@gmail.com>
2026-07-12 18:03:17 +0300

    fix: simplify middleware to standard nextjs function to avoid edge database initialization crashes

--- middleware.js
+++ middleware.js
@@ -1,14 +1,8 @@
-if (!process.env.AUTH_SECRET) {
-  process.env.AUTH_SECRET = "centstore_jwt_secret_key_2026_x_bungoma_tours_32bytes_fallback";
-}
-import authConfig from "./auth.config";
-import NextAuth from "next-auth";
 import { NextResponse } from "next/server";
-const { auth } = NextAuth(authConfig);
 
-export default auth(async (req) => {
+export default async function middleware(req) {
   const response = NextResponse.next();
   response.headers.set("x-pathname", req.nextUrl.pathname);
   response.headers.set("x-url", req.nextUrl.href);
   return response;
-});
+}
